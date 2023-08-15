00ca7c3afc5cf5793f53732c94187820bce5d357
Gabrielle Oliveira <gabrielleoliveir4@gmail.com>
2023-08-15 20:49:37 -0300

    Hurt Animation

--- My project/Assets/Script/Player/Player.cs
+++ My project/Assets/Script/Player/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
 
@@ -10,10 +11,13 @@ public class Player : MonoBehaviour
     private Transform playerTransform; 
     public static Player instance;
     [SerializeField] private int lives = 3;
+    private bool isMoving;
+    private bool isHurt;
+
 
     private void Awake()
     {
-
+        #region Singleton
         if (instance == null )
         {
             instance = this;
@@ -22,6 +26,7 @@ public class Player : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+        #endregion
 
         playerTransform = GetComponent<Transform>();
         animator = GetComponent<Animator>();
@@ -30,25 +35,71 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        Move();
+        moveHandler();
+        LifeHandler();
+        AnimationHandler();
+        
     }
 
 
 
-    
-    private void Move()
+    #region Handlers
+
+    private void moveHandler()
         
     {
         float moveX = Input.GetAxisRaw("Horizontal") * velocity * Time.deltaTime;
         float movey = Input.GetAxisRaw("Vertical") * velocity * Time.deltaTime;
         playerTransform.Translate(moveX, movey, 0);
-        print(lives);
-        if (lives <=0)
+        if (moveX !=0 || movey != 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isHurt = false;
+        }
+
+        //isMoving = moveX !=0 || movey !=0; (OUTRA ALTERNATIVA PARA POUPAR LINHAS)
+     }
+
+    private void LifeHandler()
+    {
+        if (lives <= 0)
         {
             Destroy(gameObject);
         }
+
+
     }
 
+    private void AnimationHandler()
+    {
+        bool isMovingAnimation = animator.GetBool("isMoving");
+        bool isHurtAnimation = animator.GetBool("isHurt");
+
+        if (isMoving && isMovingAnimation == false)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else if (!isMoving && isMovingAnimation == true)
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        if (isHurt && isHurtAnimation == false) 
+        {
+            animator.SetBool("isHurt", true);
+        } 
+        else if (!isHurt &&isHurtAnimation ==true)
+        {
+            animator.SetBool("isHurt", false);
+        }
+
+        
+       
+    }
+    #endregion
     public Vector3 GetPlayerPosition()
     {
         return playerTransform.position;
@@ -57,5 +108,8 @@ public class Player : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         lives--;
+        isHurt = true;
+        print(lives);
     }
+
 }
